e6e31d5daafd43d022627be4176529506c4e33ad
plettj <jlplett@uwaterloo.ca>
2024-10-29 13:59:42 +0100

    update: Responsive styling

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,9 +3,9 @@ import { gameCardList } from "./content";
 
 export default async function Home() {
   return (
-    <main className="flex flex-col gap-2 py-12 items-center">
+    <main className="flex flex-col gap-2 py-12 px-4 md:px-8 items-center">
       <h1>PLETT.FUN</h1>
-      <p>
+      <p className="text-center">
         A tiny website by{" "}
         <a
           className="no-underline text-blue-900 hover:to-blue-700"
@@ -16,14 +16,14 @@ export default async function Home() {
         .
       </p>
       <hr className="h-2" />
-      <div className="grid grid-cols-2 sm:grid-cols-3 gap-8 w-full max-w-[65ch]">
+      <div className="my-8 grid grid-cols-2 md:grid-cols-3 gap-4 md:gap-8 w-full max-w-[65ch]">
         {gameCardList.map((gameData) => (
           <GameCard key={gameData.title} {...gameData} />
         ))}
       </div>
       <a
         href="https://old.plett.dev/games"
-        className="mt-6 no-underline text-blue-900 hover:to-blue-700"
+        className="no-underline text-blue-900 hover:to-blue-700 text-center"
       >
         Old game site
       </a>
