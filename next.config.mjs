9d23a1c9585ff3420dc87efbf453dc4c05a0efd7
plettj <jlplett@uwaterloo.ca>
2024-11-30 02:03:29 +0100

    feat: Add Boggle

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  async rewrites() {
+    return [
+      // For Next.js multi-zone deployment, catch our current paths before redirecting for zones.
+      {
+        source: "/:path*",
+        destination: `/:path*`,
+      },
+      {
+        source: "/boggle",
+        // Boggle is hosted on a custom domain, so Vercel can display it to search engines.
+        destination: "https://boggle.plett.fun/boggle",
+      },
+      {
+        source: "/boggle/:path*",
+        destination: "https://boggle.plett.fun/boggle/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
